a1c40a2e47b139de9ffc0bda27cf306909b00083
Abhi <abhidevops94@gmail.com>
2025-06-01 00:26:51 +0530

    roboshop2

--- frontend.sh
+++ frontend.sh
@@ -1,38 +1,7 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-
-mkdir -p $LOGS_FOLDER
-echo "script started excuting at: $(date)" | tee -a $LOG_FILE
-
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "you are running with root access" | tee -a $LOG_FILE
-fi
-
-#Validate function takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-then
-    echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
-else
-    echo -e "$2 is ... $R failure $N" | tee -a $LOG_FILE
-    exit 1
-fi
-}
+source ./common.sh
+check_root
 
 dnf module list nginx &>>$LOG_FILE
 VALIDATE $? "listing ngnix module"
@@ -68,4 +37,6 @@ VALIDATE $? "copying nginx conf file"
 
 systemctl restart nginx
 VALIDATE $? "Restarting nginx"
+
+print_time
 
